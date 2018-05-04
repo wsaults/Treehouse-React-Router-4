6838dc941d1db213d367f982f7a69591c79eb549
Will Saults <wsaults@yahoo.com>
2018-05-04 11:41:57 -0500

    Refactored courses and improved header

--- react-router-4/src/components/Courses.js
+++ react-router-4/src/components/Courses.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import { 
+  NavLink,
+  Route,
+  Redirect
+} from 'react-router-dom';
 
-const Courses = () => (
+import CSS from './courses/CSS';
+import HTML from './courses/HTML';
+import Javascript from './courses/JavaScript';
+
+const Courses = ({match}) => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to={`${match.url}/html`}>HTML</NavLink></li>
+        <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
+        <li><NavLink to={`${match.url}/javascript`}>JavaScript</NavLink></li>
       </ul>
     </div>
     
-    {/* Write routes here... */}
+    <Route exact path={match.path} render={ () => <Redirect to={`${match.path}/html`} /> } />
+    <Route path={`${match.path}/html`} component={HTML} />
+    <Route path={`${match.path}/css`} component={CSS} />
+    <Route path={`${match.path}/javascript`} component={Javascript} />
   </div>
 );
 
--- react-router-4/src/components/Header.js
+++ react-router-4/src/components/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/teachers">Teachers</Link></li>
-      <li><Link to="/courses">Courses</Link></li>
+      <li><NavLink exact to="/">Home</NavLink></li>
+      <li><NavLink to="/about">About</NavLink></li>
+      <li><NavLink to="/teachers">Teachers</NavLink></li>
+      <li><NavLink to="/courses">Courses</NavLink></li>
     </ul>    
   </header>
 );
